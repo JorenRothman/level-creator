5d4a01dcaba2df27ca3f5d3648ffdf3fb47cdb89
Joren Rothman <rothmanjoren@gmail.com>
2018-10-26 14:45:32 +0200

    Updates

---
+++ precache-manifest.bd0bb2ab0908da0e6e1456f2048565aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28c7ba6c3f6ba04e9e7",
+    "url": "/level-creator/static/js/runtime~main.a28c7ba6.js"
+  },
+  {
+    "revision": "f3699260350534ccee4c",
+    "url": "/level-creator/static/js/main.f3699260.chunk.js"
+  },
+  {
+    "revision": "c1ff31f492a208f506d3",
+    "url": "/level-creator/static/js/1.c1ff31f4.chunk.js"
+  },
+  {
+    "revision": "f3699260350534ccee4c",
+    "url": "/level-creator/static/css/main.3c75e14c.chunk.css"
+  },
+  {
+    "revision": "3030d353b094beda190f8e4918f142c0",
+    "url": "/level-creator/index.html"
+  }
+];
